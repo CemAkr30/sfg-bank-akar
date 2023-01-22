87b11fc9a2059bb3da03790c78aca2084e8695db
= <cemakar18@gmail.com>
2023-01-22 23:55:12 +0300

    adding multiple project

---
+++ sfg-bank-web/src/main/java/ca/springframework/sfgbankakar/SfgBankAkarApplication.java
@@ -0,0 +1,13 @@
+package ca.springframework.sfgbankakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBankAkarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgBankAkarApplication.class, args);
+    }
+
+}
