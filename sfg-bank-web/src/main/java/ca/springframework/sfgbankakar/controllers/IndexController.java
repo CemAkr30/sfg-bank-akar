bcebf03d9e4c8bfbb4162260b7cfe2cef774654d
= <cemakar18@gmail.com>
2023-01-25 00:49:25 +0300

    adding Index Page and Controller. Close #

---
+++ sfg-bank-web/src/main/java/ca/springframework/sfgbankakar/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package ca.springframework.sfgbankakar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//RestController method tarafında ki string değeri bile dönerken @controller dönmüyor thymeleaf göre form dönüyor
+@RestController
+public class IndexController {
+
+    //Mapping de belirtilen türlere göre request atılır
+    @GetMapping({"","/","index","/index.html"})
+    public String index(){
+        return "WELCOME INDEX PAGE";
+    }
+}
