18a5cb1ee9e9e6ac3f9b80023f1533efa33e0158
CemAkr30 <cemakar18@gmail.com>
2023-01-29 03:37:46 +0300

    add mail sender service in project close#2

--- sfg-bank-web/src/main/java/ca/springframework/sfgbankakar/config/ControllerConfig.java
+++ sfg-bank-web/src/main/java/ca/springframework/sfgbankakar/config/ControllerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 
 import javax.mail.internet.MimeMessage;
@@ -42,6 +43,11 @@ public class ControllerConfig {
 
         return fakeDataSource;
     }
+
+//    @Bean
+//    public JavaMailSender javaMailSender() {
+//        return new JavaMailSenderImpl();
+//    }
 
 
 //    @Bean
