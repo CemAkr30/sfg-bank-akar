f34af243b29f7014796c9ab5c0e3754ea716b379
CemAkr30 <cemakar18@gmail.com>
2023-02-01 22:34:17 +0300

    adding pom,repotest

---
+++ sfg-bank-data/src/test/java/ca/springframework/sfgbankakar/repositories/KimlikRepositoryTest.java
@@ -0,0 +1,25 @@
+//package ca.springframework.sfgbankakar.repositories;
+//
+//import ca.springframework.sfgbankakar.model.Kimlik;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//import java.util.Optional;
+//
+//@DataJpaTest
+//public class KimlikRepositoryTest {
+//
+//    @Autowired
+//    KimlikRepository kimlikRepository;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//    }
+//
+//    public void findById() throws Exception{
+//        Optional<Kimlik> kimlikOptional =
+//    }
+// }
