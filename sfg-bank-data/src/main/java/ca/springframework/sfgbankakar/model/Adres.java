86b25732942359e9cbbc5cb06ac99a3e81a8cedd
CemAkr30 <cemakar18@gmail.com>
2023-02-06 01:07:22 +0300

    add/close#18

--- sfg-bank-data/src/main/java/ca/springframework/sfgbankakar/model/Adres.java
+++ sfg-bank-data/src/main/java/ca/springframework/sfgbankakar/model/Adres.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 
 
 @Data
@@ -22,6 +23,7 @@ public class Adres  extends BaseEntity {
     @Column(name = "BEYAN_ADRES")
     private String beyanAdres;
 
+    @Email
     @Column(name = "EMAIL")
     private String email;
 
---
+++ sfg-bank-web/src/main/java/ca/springframework/sfgbankakar/exception/AuthenticationException.java
@@ -0,0 +1,17 @@
+package ca.springframework.sfgbankakar.exception;
+
+public class AuthenticationException  extends  RuntimeException {
+
+
+    public AuthenticationException(){
+        super();
+    }
+
+    public AuthenticationException(String message){
+        super(message);
+    }
+
+    public AuthenticationException(String message,Throwable cause){
+        super(message, cause);
+    }
+}
